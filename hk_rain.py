44209821681944ac05d05b5ed6594da54b773f79
chekyuetwong <chekyuetwong@msn.com>
2022-06-14 20:50:15 +0100

    summary0

--- hk_rain.py
+++ hk_rain.py
@@ -15,3 +15,101 @@ browser = webdriver.Firefox(options=opts)
 
 browser.get('http://example.com')
 st.write(browser.page_source)
+
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+Title = "Measured Tide Levels"
+
+Initialisation = True
+Download = True
+Quarry_Bay = True 
+Tai_Po_Kau = True
+Tsim_Bei_Tsui = True
+Tai_O = True
+
+from datetime import datetime
+year = datetime.now().year
+month = datetime.now().month
+
+import pandas as pd
+from bs4 import BeautifulSoup
+import time
+
+import matplotlib.dates as mdates
+
+def hko_table_csv(url):
+    driver = webdriver.Firefox(options=opts)
+    driver.get(url)
+    time.sleep(5)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    mainc = soup.find(id='mainContent')
+    rows = mainc.find_all('tr')
+    data=[]
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+
+    driver.close() # closing the webdriver
+    return data
+
+def tide_data(station):
+  station=str(station)
+  try: 
+    URL="https://www.hko.gov.hk/en/tide/marine/realtide.htm?s="+station+"&t=TABLE"
+    print("Updated data retrieved from: ", URL)
+    data = hko_table_csv(URL)
+    df = pd.DataFrame(data)
+    df1 = df.iloc[8:, 0:3]
+
+    df1.columns=["Date", "Measured", "Predicted"]
+    df1=df1.set_index("Date")
+    df1.index = pd.to_datetime(df1.index)
+    #df1.to_csv(fp("test_for_today"))
+
+    df1["Measured"] = pd.to_numeric(df1["Measured"], errors='coerce')
+    df1["Predicted"]=pd.to_numeric(df1["Predicted"], errors='coerce')
+    #print(df1)
+    return df1
+
+  except:
+    print("Error encountered. The plot for "+station+" was unsuccessful.")
+
+
+fig = plt.figure(figsize=[15,5])
+ax = fig.add_subplot(1,1,1)
+
+if Quarry_Bay:
+  QUB=tide_data("QUB")
+  plt.plot(QUB.index, QUB["Measured"], label="Quarry Bay")
+
+if Tai_Po_Kau:
+  TPK=tide_data("TPK")
+  plt.plot(TPK.index, TPK["Measured"], label="Tai Po Kau")
+
+if Tsim_Bei_Tsui:
+  TBT=tide_data("TBT")
+  plt.plot(TBT.index, TBT["Measured"], label="Tsim Bei Tsui")
+if Tai_O:
+  TAO=tide_data("TAO")
+  plt.plot(TAO.index, TAO["Measured"], label="Tai O")
+
+hours = mdates.HourLocator(interval = 3)
+h_fmt = mdates.DateFormatter('%Y-%m-%d %H:%M')
+ax.set_title(Title, size=20)
+ax.xaxis.set_major_locator(hours)
+ax.xaxis.set_major_formatter(h_fmt)
+ax.grid()
+ax.set_ylabel("Tide Level (mCD)")
+
+fig.autofmt_xdate()
+
+plt.legend()
+timestamp=max(QUB.index).strftime("%Y-%m-%d_%H-%M")
+filename = Title+".png"
+#plt.savefig(filename, bbox_inches='tight')
+plt.show()
+st.pyplot(fig)
