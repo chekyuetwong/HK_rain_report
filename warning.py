91e03f24c1d9980396f37558fa1b99a35f22b43c
chekyuetwong <chekyuetwong@msn.com>
2022-06-15 10:59:33 +0100

    Update warning.py

--- warning.py
+++ warning.py
@@ -127,30 +127,31 @@ def warning():
   ---
   """)
 
-  Title = st.text_input("Timeline Title", "Event Timeline")
+  with st.sidebar:
+    Title = st.text_input("Timeline Title", "Event Timeline")
   
-  a1, a2, a3 = st.columns(3)
-  with a1:
-    Start_Date = st.date_input("Start Date")
-    Start_Time = st.time_input('Start Time', default_time1)
-  with a2:
-    End_Date = st.date_input("End Date")
-    End_Time = st.time_input('End Time',  default_time2)
+    a1, a2, a3 = st.columns(3)
+    with a1:
+      Start_Date = st.date_input("Start Date")
+      Start_Time = st.time_input('Start Time', default_time1)
+    with a2:
+      End_Date = st.date_input("End Date")
+      End_Time = st.time_input('End Time',  default_time2)
 
-  b1, b2, b3, b4, b5 = st.columns(5)
-  with b1:
-    Rainstorm = st.selectbox("Rainstorm", (0,1,2,3,4,5), 1)
-  with b2:
-    SAFNNT = st.selectbox("SAFNNT", (0,1,2,3,4,5), 2)
-  with b3:
-    Tropical_Cyclone = st.selectbox("Tropical Cyclone", (0,1,2,3,4,5), 3)
-  with b4:
-    Landslip = st.selectbox("Landslip", (0,1,2,3,4,5), 4)
-  with b5:
-    Thunderstorm = st.selectbox("Thunderstorm", (0,1,2,3,4,5), 5)
-  Image_Height = st.slider('Image Height', 5, 30, 10)
-  st.write("""#Timeline Customisation
-  """)
+    b1, b2, b3, b4, b5 = st.columns(5)
+    with b1:
+      Rainstorm = st.selectbox("Rainstorm", (0,1,2,3,4,5), 1)
+    with b2:
+      SAFNNT = st.selectbox("SAFNNT", (0,1,2,3,4,5), 2)
+    with b3:
+      Tropical_Cyclone = st.selectbox("Tropical Cyclone", (0,1,2,3,4,5), 3)
+    with b4:
+      Landslip = st.selectbox("Landslip", (0,1,2,3,4,5), 4)
+    with b5:
+      Thunderstorm = st.selectbox("Thunderstorm", (0,1,2,3,4,5), 5)
+    Image_Height = st.slider('Image Height', 5, 30, 10)
+    st.write("""#Timeline Customisation
+    """)
 
   ds=datetime.combine(Start_Date,Start_Time)
   de=datetime.combine(End_Date,End_Time)
@@ -162,11 +163,6 @@ def warning():
   Thunderstorm = pos_map(Thunderstorm)
   Landslip = pos_map(Landslip)
   Tropical_Cyclone = pos_map(Tropical_Cyclone)
-  st.write(Rainstorm)
-  st.write(SAFNNT)
-  st.write(Thunderstorm)
-  st.write(Landslip)
-  st.write(Tropical_Cyclone)
   
   year = datetime.now().year
   month = datetime.now().month
