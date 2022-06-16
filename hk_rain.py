a067b7852b86b34b6503bd8a73a913ea95ca10d1
chekyuetwong <chekyuetwong@msn.com>
2022-06-16 16:30:44 +0100

    1

--- hk_rain.py
+++ hk_rain.py
@@ -1,6 +1,6 @@
 #from tide import tide
 #from tide2 import tide2
-from daily_weather import daily_weather
+#from daily_weather import daily_weather
 #from warning import warning
 import streamlit as st
 import os, sys
@@ -16,25 +16,27 @@ import matplotlib.dates as mdates
 from streamlit import caching
 st.set_page_config(layout="wide")
 
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
 def home_page():
   st.markdown("""# Welcome to this App
   This is a web app under alpha testing regarding Hong Kong weather.
   """)
 
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 to_func = {
   "Home": home_page,
-  "Daily Weather": daily_weather,
-  #"Tide (Nearest 24 Hours)": tide,
+  #"Daily Weather": daily_weather,
+  "Tide (Nearest 24 Hours)": tide,
   #"Tide (10-min Data)": tide2,
   #"Warning Timeline":warning
   
 }
 
-#_ = installff()
+_ = installff()
 with st.sidebar:
   demo_name = st.selectbox("Applications", to_func.keys())
 to_func[demo_name]()
--- tide.py
+++ tide.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 opts = FirefoxOptions()
 opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -12,11 +14,6 @@ import matplotlib.dates as mdates
 
 @st.experimental_singleton
 def hko_table_csv(url):
-    #driver = webdriver.Firefox(options=opts)
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    driver = webdriver.Firefox(options=firefoxOptions,
-    executable_path="/home/appuser/.conda/bin/geckodriver",)
     driver.get(url)
     time.sleep(5)
     html = driver.page_source
