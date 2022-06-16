65fbdc1147e903fa14dbf32c3892537855cd23ea
chekyuetwong <chekyuetwong@msn.com>
2022-06-16 11:06:52 +0100

    Update daily_weather.py

--- daily_weather.py
+++ daily_weather.py
@@ -55,8 +55,8 @@ def daily_weather():
 
   all_col = ["Day", "Mean Pressure (hPa)", "Absolute Daily Max (deg. C)", "Mean (deg. C)", "Absolute Daily Min (deg. C)", "Mean Dew Point (deg. C)", "Mean Relative Humidity (%)", "Mean Amount", "Total Rainfall (mm)", "Total Bright Sunshine (hours)", "Prevailing Wind Direction (degrees)", "Mean Wind Speed (km/h)"] 
   dfc=pd.DataFrame(columns=all_col)
-  ind=pd.date_range(ds, de, freq='1M', closed="right")
-  #ind=pd.date_range(ds, de, freq='1M', inclusive="both") #pandas 1.4.0 or later
+  ind=pd.date_range(ds, de, freq='1M', closed="right") #replace closed by (inclusive="both") when using pandas 1.4.0 or later
+  
   
   st.write(ind)
   for i in ind:
