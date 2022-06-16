4e1604d8fdbdb0bdd9b543fc7dc1b0a7d9d15741
chekyuetwong <chekyuetwong@msn.com>
2022-06-16 17:02:20 +0100

    Update hk_rain.py

--- hk_rain.py
+++ hk_rain.py
@@ -26,7 +26,6 @@ def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
-@st.experimental_singleton
 _ = installff()
 to_func = {
   "Home": home_page,
