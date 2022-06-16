5db8fa26a6316c31930f430dafdf9db77996b3ed
chekyuetwong <chekyuetwong@msn.com>
2022-06-16 22:19:40 +0100

    Update hk_rain.py

--- hk_rain.py
+++ hk_rain.py
@@ -1,7 +1,7 @@
-#from tide import tide
-#from tide2 import tide2
+from tide import tide
+from tide2 import tide2
 #from daily_weather import daily_weather
-#from warning import warning
+from warning import warning
 import streamlit as st
 import os, sys
 from selenium import webdriver
