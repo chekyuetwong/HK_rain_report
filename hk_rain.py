f04e481c63e407e8b6c1ef313a6ae7c36ab18679
chekyuetwong <chekyuetwong@msn.com>
2022-06-16 10:52:10 +0100

    Update hk_rain.py

--- hk_rain.py
+++ hk_rain.py
@@ -28,6 +28,7 @@ def home_page():
 
 to_func = {
   "Home": home_page,
+  "Daily Weather": daily_weather,
   "Tide (Nearest 24 Hours)": tide,
   "Tide (10-min Data)": tide2,
   "Warning Timeline":warning
