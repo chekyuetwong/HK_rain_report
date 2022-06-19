244d92476fd7eeba35a03303979e77afd5a0081d
chekyuetwong <chekyuetwong@msn.com>
2022-06-19 10:11:06 +0100

    Update tide.py

--- tide.py
+++ tide.py
@@ -63,46 +63,52 @@ def tide():
   with st.sidebar:
     st.title("Measured Tide Levels (Nearest 24 hours)")
     Title = st.text_input("Plot Title", "Measured Tide Levels")
-    Quarry_Bay = st.checkbox('Quarry Bay', value=True)
-    Tai_Po_Kau = st.checkbox('Tai Po Kau', value=True)
-    Tsim_Bei_Tsui = st.checkbox('Tsim Bei Tsui', value=True)
-    Tai_O = st.checkbox('Tai O', value=True)
+    #Quarry_Bay = st.checkbox('Quarry Bay', value=True)
+    #Tai_Po_Kau = st.checkbox('Tai Po Kau', value=True)
+    #Tsim_Bei_Tsui = st.checkbox('Tsim Bei Tsui', value=True)
+    #Tai_O = st.checkbox('Tai O', value=True)
     if st.button('Refresh Data from HKO'):
       tide_data.clear()
       hko_table_csv.clear()
+  QUB=tide_data("QUB")
+  TPK=tide_data("TPK")
+  TBT=tide_data("TBT")
+  TAO=tide_data("TAO")
 
-  fig = plt.figure(figsize=[15,5])
-  ax = fig.add_subplot(1,1,1)
 
-  if Quarry_Bay:
-    QUB=tide_data("QUB")
-    plt.plot(QUB.index, QUB["Measured"], label="Quarry Bay")
 
-  if Tai_Po_Kau:
-    TPK=tide_data("TPK")
-    plt.plot(TPK.index, TPK["Measured"], label="Tai Po Kau")
+  #fig = plt.figure(figsize=[15,5])
+  #ax = fig.add_subplot(1,1,1)
 
-  if Tsim_Bei_Tsui:
-    TBT=tide_data("TBT")
-    plt.plot(TBT.index, TBT["Measured"], label="Tsim Bei Tsui")
-  if Tai_O:
-    TAO=tide_data("TAO")
-    plt.plot(TAO.index, TAO["Measured"], label="Tai O")
+  #if Quarry_Bay:
+  #  QUB=tide_data("QUB")
+  #  plt.plot(QUB.index, QUB["Measured"], label="Quarry Bay")
 
-  hours = mdates.HourLocator(interval = 3)
-  h_fmt = mdates.DateFormatter('%Y-%m-%d %H:%M')
-  ax.set_title(Title, size=20)
-  ax.xaxis.set_major_locator(hours)
-  ax.xaxis.set_major_formatter(h_fmt)
-  ax.grid()
-  ax.set_ylabel("Tide Level (mCD)")
+  #if Tai_Po_Kau:
+  #  TPK=tide_data("TPK")
+  #  plt.plot(TPK.index, TPK["Measured"], label="Tai Po Kau")
 
-  fig.autofmt_xdate()
+  #if Tsim_Bei_Tsui:
+  #  TBT=tide_data("TBT")
+  #  plt.plot(TBT.index, TBT["Measured"], label="Tsim Bei Tsui")
+  #if Tai_O:
+  #  TAO=tide_data("TAO")
+  #  plt.plot(TAO.index, TAO["Measured"], label="Tai O")
 
-  plt.legend()
+  #hours = mdates.HourLocator(interval = 3)
+  #h_fmt = mdates.DateFormatter('%Y-%m-%d %H:%M')
+  #ax.set_title(Title, size=20)
+  #ax.xaxis.set_major_locator(hours)
+  #ax.xaxis.set_major_formatter(h_fmt)
+  #ax.grid()
+  #ax.set_ylabel("Tide Level (mCD)")
+  #fig.autofmt_xdate()
+  #plt.legend()
   #timestamp=max(QUB.index).strftime("%Y-%m-%d_%H-%M")
-  filename = Title+".png"
+  #filename = Title+".png"
   #plt.savefig(filename, bbox_inches='tight')
   #plt.show()
-  st.pyplot(fig)
+  fig = px.line(x=QUB.index, y=[QUB, TPK, TBT, TAO])
+  fig.update_layout(autotypenumbers='convert types', width=1200, height=600)
+  st.plotly_chart(fig)
   st.write("Data Source: https://www.hko.gov.hk/en/tide/marine/realtide.htm")
