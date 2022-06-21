3841008a61e51709e7eec41ecd32ae76e9060760
chekyuetwong <chekyuetwong@msn.com>
2022-06-22 00:00:54 +0100

    Update hk_rain.py

--- hk_rain.py
+++ hk_rain.py
@@ -23,7 +23,7 @@ def home_page():
 
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
 try:
   from tide import tide
